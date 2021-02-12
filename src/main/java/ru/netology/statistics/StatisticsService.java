838da8b266f5eda117de3ffe4bb5af3fef054faa
Natalya <76731572+Arenzon@users.noreply.github.com>
2021-02-12 13:55:41 +0300

    Update StatisticsService.java

--- src/main/java/ru/netology/statistics/StatisticsService.java
+++ src/main/java/ru/netology/statistics/StatisticsService.java
@@ -3,72 +3,79 @@ package ru.netology.statistics;
 public class StatisticsService {
 
 
-        public long calculateSum(long[] sales) {
-            long sum = 0;
-            for (long sale : sales) {
-                sum += sale;
-            }
-            return sum;
+        public int calculateSum ( int[] sales ) {
+        int sum = 0;
+        for (int sale : sales) {
+            sum += sale;
         }
-
-        public float arithmeticMean(long[] sales) {
-
-            float average = calculateSum(sales)/sales.length;
-
-            return average;
-
-        }
-        public long monthOfMaxSale(long[] sales){
-            int month = 0;
-            int n=0;
-            long currentMax = sales[0];
-            for (long sale : sales) {
-                if (currentMax <= sale){
-                    currentMax = sale;
-                    n=month;
-                }
-                month++;
-            }
-            n++;
-            return n;
-
-        }
-        public long monthOfMinSale(long[] sales){
-            int month = 0;
-            int n=0;
-            long currentMin = sales[0];
-            for (long sale : sales) {
-                if (currentMin > sale){
-                    currentMin = sale;
-                    n=month;
-                }
-                month++;
-            }
-            n++;
-            return n;
-
-        }
-
-        public long sumMonthMinMean(long[] sales){
-            int sumMonth = 0;
-            float arithmeticMean = calculateSum(sales)/sales.length;
-            for (long sale : sales) {
-                if (arithmeticMean > sale){
-                    sumMonth++;
-                }
-            }
-            return sumMonth;
-
-        }
-
-        public long sumMonthMaxMean(long[] sales){
-            int sumMonth = 0;
-            float arithmeticMean = calculateSum(sales)/sales.length;
-            for (long sale : sales) {
-                if (arithmeticMean < sale){
-                    sumMonth++;
-                }
-            }
-            return sumMonth;
-        }
+        return sum;
     }
+
+    public int arithmeticMean ( int[] sales ) {
+
+        int average = calculateSum(sales) / sales.length;
+
+        return average;
+
+    }
+
+    public int monthOfMaxSale ( int[] sales ) {
+        int month = 0;
+        int n = 0;
+        int currentMax = sales[0];
+        for (int sale : sales) {
+            if (currentMax <= sale) {
+                currentMax = sale;
+                n = month;
+            }
+            month++;
+        }
+        n++;
+        return n;
+
+    }
+
+    public int monthOfMinSale ( int[] sales ) {
+        int month = 0;
+        int n = 0;
+        int currentMin = sales[0];
+        for (int sale : sales) {
+            if (currentMin > sale) {
+                currentMin = sale;
+                n = month;
+            }
+            month++;
+        }
+        n++;
+        return n;
+
+    }
+
+    public int sumMonthMinMean ( int[] sales ) {
+        int average = average(sales);
+        int sumMonth = 0;
+        for (int sale : sales) {
+            if (average > sale) {
+                sumMonth++;
+            }
+        }
+        return sumMonth;
+
+    }
+
+    private int average ( int[] sales ) {
+        return calculateSum(sales) / sales.length;
+
+    }
+
+    public int sumMonthMaxMean ( int[] sales ) {
+        int average = average(sales);
+        int sumMonth = 0;
+        for (int sale : sales) {
+            if (average < sale) {
+                sumMonth++;
+            }
+        }
+        return sumMonth;
+    }
+}
